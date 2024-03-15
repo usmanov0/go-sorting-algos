ec6b29734026c8d8c199c039226cafce916be7c4
usmanov <azikusmanov882@gmail.com>
2024-03-15 23:04:39 +0500

    merge sort

---
+++ merge/merge_sort.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func mergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := make([]int, 0, len(left)+len(right))
+
+	for len(left) > 0 || len(right) > 0 {
+		if len(left) == 0 {
+			return append(result, right...)
+		}
+		if len(right) == 0 {
+			return append(result, left...)
+		}
+
+		if left[0] <= right[0] {
+			result = append(result, left[0])
+			left = left[1:]
+		} else {
+			result = append(result, right[0])
+			right = right[1:]
+		}
+	}
+
+	return result
+}
+
+func main() {
+	arr := []int{64, 34, 25, 12, 22, 11, 90}
+	fmt.Println("Unsorted array: ", arr)
+	arr = mergeSort(arr)
+	fmt.Println("Sorted array: ", arr)
+}
